596545edb01d2f920041a348283002769c4eeb34
Hugo Torres Martínez <huguitorres66@gmail.com>
2023-02-07 11:37:01 +0100

    Codigo semilla

---
+++ vertex.c
@@ -0,0 +1,60 @@
+#include "vertex.h"
+#include <string.h>
+
+
+/*----------------------------------------------------------------------------------------*/
+/*
+Private function:
+*/
+Status vertex_setField (Vertex *v, char *key, char *value);
+
+Status vertex_setField (Vertex *v, char *key, char *value) {
+  if (!key || !value) return ERROR;
+
+  if (strcmp(key, "id") == 0) {
+    return vertex_setId(v, atol(value));
+  } else if (strcmp(key, "tag") == 0) {
+    return vertex_setTag(v, value);
+  } else if (strcmp(key, "state") == 0) {
+    return vertex_setState(v, (Label)atoi(value));
+  }
+
+  return ERROR;
+}
+
+/*----------------------------------------------------------------------------------------*/
+Vertex *vertex_initFromString(char *descr){
+  char buffer[1024];
+  char *token;
+  char *key;
+  char *value;
+  char *p;
+  Vertex *v;
+
+  /* Check args: */
+  if (!descr) return NULL;
+
+  /* Allocate memory for vertex: */
+  v = vertex_init();
+  if (!v) return NULL;
+
+  /* Read and tokenize description: */
+  sprintf(buffer, "%s", descr);
+  token = strtok(buffer, " \t\n");
+  while (token) {
+    p = strchr(token, ':');
+    if (!p) continue;
+
+    *p = '\0';
+    key = token;
+    value = p+1;
+
+    vertex_setField(v, key, value);
+
+    token = strtok(NULL, " \t\n");
+  }
+
+  return v;
+}
+
+/**  rest of the functions in vertex.h **/
