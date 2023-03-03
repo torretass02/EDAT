ab648baf16582bbc78b29391e6cc2725c3f8eed1
Hugo Toores Martínez <huguitorres66@gmail.com>
2023-03-03 12:45:40 +0100

    EMPEZAMOS

---
+++ p2_e1a.c
@@ -0,0 +1,13 @@
+#include "libstack.a"
+#include "stack.h"
+
+
+/**
+ * @brief: Merges two stacks
+ *
+ * @param sin1, first input stack
+ * @param sin2, second input stack
+ * @param sout, result stack
+ * @return The function returns OK or ERROR
+**/
+Status mergeStacks (Stack *sin1, Stack *sin2, Stack *sout);
